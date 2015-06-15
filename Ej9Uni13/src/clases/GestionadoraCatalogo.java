2ebf91d9e0f0bdff6340831638617eec756202c6
PabloLM <P4blolm@gmail.com>
2015-06-15 20:00:09 +0200

    Estudiadas las interfaces de la clase gestionadoraCatalogo

--- Ej9Uni13/src/clases/GestionadoraCatalogo.java
+++ Ej9Uni13/src/clases/GestionadoraCatalogo.java
@@ -4,32 +4,43 @@ import java.io.File;
 
 public class GestionadoraCatalogo {
 	
-	private static File Catalogo=new File("Catalogo.dat"); 
+	
 	
 	/**Interfaz
-	 * Comentario:
-	 * Cabecera:public static void infoLibro(String Catalogo,LibroImp l)
-	 * Precondiciones:
-	 * Entrada:
-	 * Salida:
-	 * Postcondiciones:
+	 * Comentario:Devuelve el Libro que corresponda al ISBN indicado
+	 * Cabecera:public static LibroImp obtenerLibroISBN (ISBN cualquiera)
+	 * Precondiciones:no tiene
+	 * Entrada:un ISBN
+	 * Salida:un objeto de tipo LibroImp
+	 * Postcondiciones:Se devolverá ASN un objeto de tipo LibroImp cuyo ISBN sea el indicado anteriormente por parámetros  
 	 */
+	
+	
+	
 	/**Interfaz
-	 * Comentario:
-	 * Cabecera:
-	 * Precondiciones:
-	 * Entrada:
-	 * Salida:
-	 * Postcondiciones:
+	 * Comentario:Devuelve un ArrayList<LibroImp> que contendrá los libros escritos por el autor indicado por parámetros
+	 * Cabecera:public satic ArrayList<LibroImp>obtenerLibroAutor(String autor)
+	 * Precondiciones:no tiene
+	 * Entrada:una cadena que representa el nombre del autor 
+	 * Salida:un ArrayList<LibroImp>
+	 * Postcondiciones:Se devolverá ASN un ArrayList<LibroImp> que contendrá los libros escritos por el autor indicado por parámetros
 	 */
+	
+	
+	
 	/**Interfaz
-	 * Comentario:
-	 * Cabecera:
-	 * Precondiciones:
-	 * Entrada:
-	 * Salida:
-	 * Postcondiciones:
+	 * Comentario:Devuelve un ArrayList<LibroImp> con todos los libros que tengan el titulo indicado por parámetros 
+	 * Cabecera:public satic ArrayList<LibroImp>obtenerLibroTitulo(String titulo)
+	 * Precondiciones:no tiene 
+	 * Entrada:una cadena que representa el titulo del libro 
+	 * Salida:un ArrayList<LibroImp>
+	 * Postcondiciones:Se devolverá ASN un ArrayList<LibroImp> que cotnendrá los libros con el titulo indicado por parámetros 
 	 */
+	
+	
+	
+	
+	
 	/**Interfaz
 	 * Comentario:
 	 * Cabecera:
