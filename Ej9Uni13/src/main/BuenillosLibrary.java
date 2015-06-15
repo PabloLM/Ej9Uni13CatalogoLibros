1a1544e1a94896ece90062da641c436e639170d8
PabloLM <P4blolm@gmail.com>
2015-06-15 20:31:06 +0200

    Hecho el PG del main

---
+++ Ej9Uni13/src/main/BuenillosLibrary.java
@@ -0,0 +1,50 @@
+package main;
+
+/**PG
+ * 
+ * Inicio
+ * 
+ * repetir
+ * mostrarMenu y validar opcion 
+ * 	si opcion no es salir 
+ * 		segun opcion
+ * 			caso 1
+ * 				Realizar consultas sobre los libros 
+ * 					mostrarMenu y validar opcion 
+ * 						si opcion no es salir 
+ * 							segun opcion
+ * 								caso 1
+ * 									obtenerLibroISBN
+ * 								caso 2 
+ * 									obtenerLibroAutor
+ * 								caso 3
+ * 									obtenerLibroTitulo
+ * 							fin segun
+ * 						fin si		
+ * 			caso 2
+ * 				Dar de alta un libro en el catálogo
+ * 			caso 3
+ * 				Dar de baja un libro en el catálogo
+ * 			caso 4
+ * 				Calcular el precio de todos los libros de la libreria 
+ * 			caso 5 
+ * 				mostrarLibreria //esto deberia ser un metodo to string de la clase catalogo
+ * 		fin segun
+ *  finsi
+ *  
+ *  mostrarMenu y validar opcion 
+ *  
+ *  mientras no quiera salir
+ *  
+ *  FIN
+ **/
+
+
+public class BuenillosLibrary {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
