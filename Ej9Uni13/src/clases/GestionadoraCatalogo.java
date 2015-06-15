21502950b25999c1bbc96cd69bbf2dd994ee8e8c
PabloLM <P4blolm@gmail.com>
2015-06-15 20:44:19 +0200

    Mejorada gestionadoraCatalogo

--- Ej9Uni13/src/clases/GestionadoraCatalogo.java
+++ Ej9Uni13/src/clases/GestionadoraCatalogo.java
@@ -1,6 +1,7 @@
 package clases;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class GestionadoraCatalogo {
 	
@@ -15,29 +16,38 @@ public class GestionadoraCatalogo {
 	 * Postcondiciones:Se devolverá ASN un objeto de tipo LibroImp cuyo ISBN sea el indicado anteriormente por parámetros  
 	 */
 	
-	
+	public static LibroImp obtenerLibroISBN (int referencia){
+		return null;
+		
+	}
 	
 	/**Interfaz
 	 * Comentario:Devuelve un ArrayList<LibroImp> que contendrá los libros escritos por el autor indicado por parámetros
-	 * Cabecera:public satic ArrayList<LibroImp>obtenerLibroAutor(String autor)
+	 * Cabecera:public static ArrayList<LibroImp>obtenerLibroAutor(String autor)
 	 * Precondiciones:no tiene
 	 * Entrada:una cadena que representa el nombre del autor 
 	 * Salida:un ArrayList<LibroImp>
 	 * Postcondiciones:Se devolverá ASN un ArrayList<LibroImp> que contendrá los libros escritos por el autor indicado por parámetros
 	 */
 	
-	
+	public static ArrayList<LibroImp>obtenerLibroAutor(String autor){
+		return null;
+		
+	}
 	
 	/**Interfaz
 	 * Comentario:Devuelve un ArrayList<LibroImp> con todos los libros que tengan el titulo indicado por parámetros 
-	 * Cabecera:public satic ArrayList<LibroImp>obtenerLibroTitulo(String titulo)
+	 * Cabecera:public static ArrayList<LibroImp>obtenerLibroTitulo(String titulo)
 	 * Precondiciones:no tiene 
 	 * Entrada:una cadena que representa el titulo del libro 
 	 * Salida:un ArrayList<LibroImp>
 	 * Postcondiciones:Se devolverá ASN un ArrayList<LibroImp> que cotnendrá los libros con el titulo indicado por parámetros 
 	 */
 	
-	
+	public static ArrayList<LibroImp>obtenerLibroTitulo(String titulo){
+		return null;
+		
+	}
 	
 	
 	
